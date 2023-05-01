19103db7f391e99aeda0db325c8e60b22446217e
Arif Hossan <arif.hossan.eu@gmail.com>
2023-05-01 21:08:07 +0600

    axios data load and rechart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Pricing from './Pricing/Pricing';
 import AssignmentMarks from './components/AssignmentMarks/AssignmentMarks';
+import PhoneBar from './components/PhoneBar/PhoneBar';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <h1 className='text-4xl bold underline'>Hello from TailWind</h1>
       <Pricing></Pricing>
       <AssignmentMarks></AssignmentMarks>
+      <PhoneBar></PhoneBar>
     </div>
   );
 }
